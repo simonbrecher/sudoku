802ce565e76c06ef27b8d4db6d39411004eb6f25
simonbrecher <brecher.simon@gmail.com>
2022-01-14 12:34:53 +0100

    Updated tournament.

--- main.ts
+++ main.ts
@@ -4,46 +4,83 @@ function tournamentSudokuSmall(taskNum: number, solutionNum: number): void {
     SudokuBuilder.rectangular(true, 3, 2);
     let sudoku1 = SudokuBuilder.build();
     let sudoku2 = SudokuBuilder.build();
-    let sudoku3 = SudokuBuilder.build();
 
     SudokuBuilder.diagonal(true);
-    let sudokuDiagonal1 = SudokuBuilder.build();
-    let sudokuDiagonal2 = SudokuBuilder.build();
+    let sudokuDiagonal = SudokuBuilder.build();
 
     SudokuBuilder.diagonal(false);
+    SudokuBuilder.irregular(true);
+    let irregularSudoku = SudokuBuilder.build();
+
+    SudokuBuilder.rectangular(true, 3, 2);
     SudokuBuilder.vxSum(true, [[5, "V"], [10, "X"]]);
     let sudokuVx1 = SudokuBuilder.build();
 
-    SudokuBuilder.diagonal(true);
-    let sudokuVxDiagonal = SudokuBuilder.build();
-
     SudokuBuilder.vxSum(false);
-    SudokuBuilder.size(5);
+    SudokuBuilder.size(6);
     SudokuBuilder.diagonal(false);
     let noRectangle = SudokuBuilder.build();
 
     SudokuBuilder.diagonal(true);
     let noRectangleDiagonal = SudokuBuilder.build();
 
-    SudokuBuilder.rectangular(true, 3, 2);
-    SudokuBuilder.kropki(true);
+
     SudokuBuilder.diagonal(false);
-    let sudokuKropki = SudokuBuilder.build();
+    SudokuBuilder.rectangular(true, 3, 2);
+
+    let notUseKropki = Math.floor(Math.random() * 3);
+
+    let kropki, minusOne, inequality;
+    if (notUseKropki !== 0) {
+        SudokuBuilder.kropki(true);
+        kropki = SudokuBuilder.build();
+    }
+    if (notUseKropki !== 1) {
+        SudokuBuilder.minusOne(true);
+        minusOne = SudokuBuilder.build();
+    }
+    if (notUseKropki !== 2) {
+        SudokuBuilder.inequality(true);
+        inequality = SudokuBuilder.build();
+    }
 
     SudokuBuilder.kropki(false);
-    SudokuBuilder.diagonal(false);
+    SudokuBuilder.minusOne(false);
+    SudokuBuilder.inequality(false);
+    SudokuBuilder.prompterNum(null, 0);
+    SudokuBuilder.killer(true, [[1, 1], [2, 9], [3, 3], [4, 2]]);
+    let killerSudoku = SudokuBuilder.build();
+
+    SudokuBuilder.prompterNum(null, null);
+    SudokuBuilder.killer(false, null);
+    SudokuBuilder.pieceMoves(true, false);
+    let kingSudoku = SudokuBuilder.build();
+
+    SudokuBuilder.pieceMoves(false, true);
+    let knightSudoku = SudokuBuilder.build();
+
+    SudokuBuilder.pieceMoves(false, false);
 
     for (let i = 0; i < taskNum; i++) {
         Renderer.render(sudoku1?.task, sudoku1);
         Renderer.render(sudoku2?.task, sudoku2);
-        Renderer.render(sudoku3?.task, sudoku3);
-        Renderer.render(sudokuDiagonal1?.task, sudokuDiagonal1);
-        Renderer.render(sudokuDiagonal2?.task, sudokuDiagonal2);
+        Renderer.render(sudokuDiagonal?.task, sudokuDiagonal);
+        Renderer.render(irregularSudoku?.task, irregularSudoku);
         Renderer.render(sudokuVx1?.task, sudokuVx1);
-        Renderer.render(sudokuVxDiagonal?.task, sudokuVxDiagonal);
         Renderer.render(noRectangle?.task, noRectangle);
         Renderer.render(noRectangleDiagonal?.task, noRectangleDiagonal);
-        Renderer.render(sudokuKropki?.task, sudokuKropki);
+        if (notUseKropki !== 0) {
+            Renderer.render(kropki?.task, kropki);
+        }
+        if (notUseKropki !== 1) {
+            Renderer.render(minusOne?.task, minusOne);
+        }
+        if (notUseKropki !== 2) {
+            Renderer.render(inequality?.task, inequality);
+        }
+        Renderer.render(killerSudoku?.task, killerSudoku);
+        Renderer.render(kingSudoku?.task, kingSudoku);
+        Renderer.render(knightSudoku?.task, knightSudoku);
 
         Renderer.breakPage();
     }
@@ -51,14 +88,23 @@ function tournamentSudokuSmall(taskNum: number, solutionNum: number): void {
     for (let i = 0; i < solutionNum; i++) {
         Renderer.render(sudoku1?.solution, sudoku1);
         Renderer.render(sudoku2?.solution, sudoku2);
-        Renderer.render(sudoku3?.solution, sudoku3);
-        Renderer.render(sudokuDiagonal1?.solution, sudokuDiagonal1);
-        Renderer.render(sudokuDiagonal2?.solution, sudokuDiagonal2);
+        Renderer.render(sudokuDiagonal?.solution, sudokuDiagonal);
+        Renderer.render(irregularSudoku?.solution, irregularSudoku);
         Renderer.render(sudokuVx1?.solution, sudokuVx1);
-        Renderer.render(sudokuVxDiagonal?.solution, sudokuVxDiagonal);
         Renderer.render(noRectangle?.solution, noRectangle);
         Renderer.render(noRectangleDiagonal?.solution, noRectangleDiagonal);
-        Renderer.render(sudokuKropki?.solution, sudokuKropki);
+        if (notUseKropki !== 0) {
+            Renderer.render(kropki?.solution, kropki);
+        }
+        if (notUseKropki !== 1) {
+            Renderer.render(minusOne?.solution, minusOne);
+        }
+        if (notUseKropki !== 2) {
+            Renderer.render(inequality?.solution, inequality);
+        }
+        Renderer.render(killerSudoku?.solution, killerSudoku);
+        Renderer.render(kingSudoku?.solution, kingSudoku);
+        Renderer.render(knightSudoku?.solution, knightSudoku);
 
         Renderer.breakPage();
     }
@@ -67,48 +113,93 @@ function tournamentSudokuSmall(taskNum: number, solutionNum: number): void {
 function tournamentSudokuBig(taskNum: number, solutionNum: number): void {
     Renderer.perPage(2, 3);
 
+    SudokuBuilder.rectangular(true, 4, 2);
+    let sudoku1 = SudokuBuilder.build();
     SudokuBuilder.prompterNum(25, null);
     SudokuBuilder.rectangular(true, 3, 3);
-    let sudoku1 = SudokuBuilder.build();
     let sudoku2 = SudokuBuilder.build();
 
-    SudokuBuilder.prompterNum(23, null);
+    SudokuBuilder.prompterNum(22, null);
     SudokuBuilder.diagonal(true);
     let sudokuDiagonal1 = SudokuBuilder.build();
 
-    SudokuBuilder.prompterNum(null, null);
     SudokuBuilder.diagonal(false);
+    SudokuBuilder.prompterNum(23, null);
+    SudokuBuilder.irregular(true);
+    let irregularSudoku = SudokuBuilder.build();
+
+    SudokuBuilder.prompterNum(null, null);
+    SudokuBuilder.rectangular(true, 3, 3);
     SudokuBuilder.vxSum(true, [[5, "V"], [10, "X"]]);
     let sudokuVx1 = SudokuBuilder.build();
 
-    SudokuBuilder.diagonal(true);
-    let sudokuVxDiagonal = SudokuBuilder.build();
-
-    SudokuBuilder.vxSum(false);
-    SudokuBuilder.size(7);
-    SudokuBuilder.diagonal(false);
+    SudokuBuilder.vxSum(false, null);
+    SudokuBuilder.size(8);
+    SudokuBuilder.prompterNum(26, null);
     let noRectangle = SudokuBuilder.build();
 
     SudokuBuilder.diagonal(true);
+    SudokuBuilder.prompterNum(21, null);
     let noRectangleDiagonal = SudokuBuilder.build();
 
-    SudokuBuilder.rectangular(true, 3, 3);
-    SudokuBuilder.kropki(true);
+    SudokuBuilder.prompterNum(null, null);
     SudokuBuilder.diagonal(false);
-    let sudokuKropki = SudokuBuilder.build();
 
+    let notKropki = Math.floor(Math.random() * 3);
+
+    let kropki, minusOne, inequality;
+    if (notKropki !== 0) {
+        SudokuBuilder.rectangular(true, 3, 3);
+        SudokuBuilder.kropki(true);
+        kropki = SudokuBuilder.build();
+    }
+    if (notKropki !== 1) {
+        SudokuBuilder.size(8);
+        SudokuBuilder.minusOne(true);
+        minusOne = SudokuBuilder.build();
+    }
+    if (notKropki !== 2) {
+        SudokuBuilder.size(8);
+        SudokuBuilder.inequality(true);
+        inequality = SudokuBuilder.build();
+    }
+
+    SudokuBuilder.rectangular(true, 3, 3);
     SudokuBuilder.kropki(false);
-    SudokuBuilder.diagonal(false);
+    SudokuBuilder.minusOne(false);
+    SudokuBuilder.inequality(false);
+    SudokuBuilder.prompterNum(null, 1);
+    SudokuBuilder.killer(true, [[1, 6], [2, 11], [3, 11], [4, 5]]);
+    let killerSudoku = SudokuBuilder.build();
+
+    let randomPieceMoves = Math.floor(Math.random() * 2);
+
+    SudokuBuilder.killer(false, null);
+    SudokuBuilder.prompterNum(null, null);
+    SudokuBuilder.pieceMoves(randomPieceMoves === 0, randomPieceMoves === 1);
+    let piecesSudoku = SudokuBuilder.build();
+
+    SudokuBuilder.pieceMoves(false, false);
 
     for (let i = 0; i < taskNum; i++) {
         Renderer.render(sudoku1?.task, sudoku1);
         Renderer.render(sudoku2?.task, sudoku2);
         Renderer.render(sudokuDiagonal1?.task, sudokuDiagonal1);
+        Renderer.render(irregularSudoku?.task, irregularSudoku);
         Renderer.render(sudokuVx1?.task, sudokuVx1);
-        Renderer.render(sudokuVxDiagonal?.task, sudokuVxDiagonal);
         Renderer.render(noRectangle?.task, noRectangle);
         Renderer.render(noRectangleDiagonal?.task, noRectangleDiagonal);
-        Renderer.render(sudokuKropki?.task, sudokuKropki);
+        if (notKropki !== 0) {
+            Renderer.render(kropki?.task, kropki);
+        }
+        if (notKropki !== 1) {
+            Renderer.render(minusOne?.task, minusOne);
+        }
+        if (notKropki !== 2) {
+            Renderer.render(inequality?.task, inequality);
+        }
+        Renderer.render(killerSudoku?.task, killerSudoku);
+        Renderer.render(piecesSudoku?.task, piecesSudoku);
 
         Renderer.breakPage();
     }
@@ -117,11 +208,21 @@ function tournamentSudokuBig(taskNum: number, solutionNum: number): void {
         Renderer.render(sudoku1?.solution, sudoku1);
         Renderer.render(sudoku2?.solution, sudoku2);
         Renderer.render(sudokuDiagonal1?.solution, sudokuDiagonal1);
+        Renderer.render(irregularSudoku?.solution, irregularSudoku);
         Renderer.render(sudokuVx1?.solution, sudokuVx1);
-        Renderer.render(sudokuVxDiagonal?.solution, sudokuVxDiagonal);
         Renderer.render(noRectangle?.solution, noRectangle);
         Renderer.render(noRectangleDiagonal?.solution, noRectangleDiagonal);
-        Renderer.render(sudokuKropki?.solution, sudokuKropki);
+        if (notKropki !== 0) {
+            Renderer.render(kropki?.solution, kropki);
+        }
+        if (notKropki !== 1) {
+            Renderer.render(minusOne?.solution, minusOne);
+        }
+        if (notKropki !== 2) {
+            Renderer.render(inequality?.solution, inequality);
+        }
+        Renderer.render(killerSudoku?.solution, killerSudoku);
+        Renderer.render(piecesSudoku?.solution, piecesSudoku);
 
         Renderer.breakPage();
     }
@@ -144,11 +245,14 @@ function tournamentLogicSmall(taskNum: number, solutionNum: number): void {
 
     Renderer.perPage(2, 3);
     SudokuBuilder.size(5);
-    SudokuBuilder.abc(true, 2);
+    SudokuBuilder.abc(true, Math.random() < 0.5 ? 2 : 3);
     let abc1 = SudokuBuilder.build();
-    SudokuBuilder.size(5);
-    SudokuBuilder.abc(true, 3);
+    let isKing = Math.floor(Math.random() * 2) === 0;
+    SudokuBuilder.pieceMoves(isKing, ! isKing);
+    SudokuBuilder.abc(true, Math.random() < 0.4 ? 2 : 3);
     let abc2 = SudokuBuilder.build();
+
+    SudokuBuilder.pieceMoves(false, false);
 
     for (let i = 0; i < taskNum; i++) {
         // @ts-ignore
@@ -223,13 +327,20 @@ function tournamentLogicBig(taskNum: number, solutionNum: number): void {
 
     let slitherlink1 = SlitherlinkBuilder.build(10, 10);
 
+    let fourSixFirst = Math.floor(Math.random() * 2);
+    let isKing = Math.floor(Math.random() * 2);
+
     Renderer.perPage(2, 3);
-    SudokuBuilder.size(6);
-    SudokuBuilder.abc(true, 4);
+    SudokuBuilder.size(fourSixFirst === 0 ? 6 : 7);
+    SudokuBuilder.abc(true, fourSixFirst === 0 ? 4 : 3);
     let abc1 = SudokuBuilder.build();
-    SudokuBuilder.size(7);
-    SudokuBuilder.abc(true, 3);
+    SudokuBuilder.size(fourSixFirst === 0 ? 7 : 6);
+    SudokuBuilder.abc(true, fourSixFirst === 0 ? 3 : 4);
+    SudokuBuilder.pieceMoves(isKing === 1, isKing === 0);
     let abc2 = SudokuBuilder.build();
+
+    SudokuBuilder.abc(false, null);
+    SudokuBuilder.pieceMoves(false, false);
 
     for (let i = 0; i < taskNum; i++) {
         // @ts-ignore
@@ -284,45 +395,44 @@ function tournament(taskNum: number, solutionNum: number): void {
 function main(): void {
     console.log("Hello world");
 
-    // tournament(3, 1);
+    tournament(2, 1);
 
     // ---
 
-    // Basic sudoku and variants
-    SudokuBuilder.rectangular(true, 3, 2); // basic sudoku
-    // SudokuBuilder.irregular(true); // irregular blocks
+    // // Basic sudoku and variants
+    // SudokuBuilder.rectangular(true, 3, 2); // basic sudoku
     // SudokuBuilder.size(9); // without rectangles
-    // SudokuBuilder.diagonal(true); // add diagonal
-    // SudokuBuilder.kropki(true); // kropki
-    // SudokuBuilder.minusOne(true); // show all squares with difference of 1
-    // SudokuBuilder.inequality(true); // show all inequalities
-    // SudokuBuilder.killer(true, [[2, 11], [3, 9], [4, 8]]); // killer 3x3
-    SudokuBuilder.prompterNum(null, 0);
-    SudokuBuilder.killer(true, [[1, 1], [2, 9], [3, 3], [4, 2]]); // killer 3x2
-    // SudokuBuilder.vxSum(true, [[5, "V"], [10, "X"]]); SudokuBuilder.prompterNum(null, 3); // vx
-    // SudokuBuilder.pieceMoves(true, false); // king move
-    // SudokuBuilder.pieceMoves(false, true); // knight move
-
-    Renderer.perPage(3, 4);
-    for (let i = 0; i < 24; i++) {
-        let sudoku = SudokuBuilder.build();
-        if (sudoku !== null) {
-            Renderer.render(sudoku.task, sudoku);
-            // Renderer.render(sudoku.solution, sudoku);
-        }
-    }
-
-    SudokuBuilder.prompterNum(null, 1);
-    SudokuBuilder.rectangular(true, 3, 3);
-    SudokuBuilder.killer(true, [[1, 6], [2, 11], [3, 11], [4, 5]]); // killer 3x3
-    Renderer.perPage(2, 3);
-    for (let i = 0; i < 12; i++) {
-        let sudoku = SudokuBuilder.build();
-        if (sudoku !== null) {
-            Renderer.render(sudoku.task, sudoku);
-            // Renderer.render(sudoku.solution, sudoku);
-        }
-    }
+    // SudokuBuilder.irregular(true); // irregular blocks
+    // // SudokuBuilder.diagonal(true); // add diagonal
+    // // SudokuBuilder.kropki(true); // kropki
+    // // SudokuBuilder.minusOne(true); // show all squares with difference of 1
+    // // SudokuBuilder.inequality(true); // show all inequalities
+    // // SudokuBuilder.prompterNum(null, 0);
+    // // SudokuBuilder.killer(true, [[1, 1], [2, 9], [3, 3], [4, 2]]); // killer 3x2
+    // // SudokuBuilder.killer(true, [[1, 6], [2, 11], [3, 11], [4, 5]]); // killer 3x3
+    // // SudokuBuilder.vxSum(true, [[5, "V"], [10, "X"]]); SudokuBuilder.prompterNum(null, 3); // vx
+    // // SudokuBuilder.pieceMoves(true, false); // king move
+    // // SudokuBuilder.pieceMoves(false, true); // knight move
+    // //
+    //
+    // for (let j = 0; j < 1; j++) {
+    //
+    //     let then = (new Date()).getTime();
+    //
+    //     // SudokuBuilder.size(9);
+    //     Renderer.perPage(2, 3);
+    //     for (let i = 0; i < 10; i++) {
+    //         let sudoku = SudokuBuilder.build();
+    //         if (sudoku !== null) {
+    //             Renderer.render(sudoku.task, sudoku);
+    //             // Renderer.render(sudoku.solution, sudoku);
+    //         }
+    //     }
+    //
+    //     let now = (new Date()).getTime();
+    //     console.log(`${now - then}ms`);
+    //
+    // }
 
     // ---
 
